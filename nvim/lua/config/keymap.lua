8e7e62e6965a427ec3a53e9d52a0d80e2f15e8aa
Jacob <botimerj@gmail.com>
2026-05-01 16:16:05 -0400

    telescope fix

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -35,12 +35,12 @@ vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
 vim.keymap.set('n', '<Leader>E', ':Neotree toggle<CR>', { noremap = true, silent = true })
 
 -- Telescope 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<Leader>ff', builtin.find_files, { noremap = true, silent = true, desc = "Find Files" })
-vim.keymap.set('n', '<Leader>fg', builtin.git_files, { noremap = true, silent = true, desc = "Git Files" })
-vim.keymap.set('n', '<Leader>b', builtin.buffers, { noremap = true, silent = true, desc = "Buffers" })
-vim.keymap.set('n', '<Leader>*', builtin.grep_string, { noremap = true, silent = true, desc = "Grep String" })
-vim.keymap.set('n', '<Leader>/', builtin.live_grep, { noremap = true, silent = true, desc = "Grep" })
+-- local builtin = require('telescope.builtin')
+-- vim.keymap.set('n', '<Leader>ff', builtin.find_files, { noremap = true, silent = true, desc = "Find Files" })
+-- vim.keymap.set('n', '<Leader>fg', builtin.git_files, { noremap = true, silent = true, desc = "Git Files" })
+-- vim.keymap.set('n', '<Leader>b', builtin.buffers, { noremap = true, silent = true, desc = "Buffers" })
+-- vim.keymap.set('n', '<Leader>*', builtin.grep_string, { noremap = true, silent = true, desc = "Grep String" })
+-- vim.keymap.set('n', '<Leader>/', builtin.live_grep, { noremap = true, silent = true, desc = "Grep" })
 
 -- Code Companion
 vim.api.nvim_set_keymap('n', '<leader>cc', ':CodeCompanionChat toggle<CR>', { noremap = true, silent = true })
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,26 +1,88 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-        pickers = {
-            find_files = {
-                follow = true,
-            },
-            live_grep = {
-                follow = true,
-            },
-        }, -- pickers
-        defaults = {
-            mappings = {
-                i = {
-                    ['<C-d>'] = require('telescope.actions').delete_buffer,
-                }, -- i
-                n = {
-                    ['d'] = require('telescope.actions').delete_buffer,
-                    ['q'] = require('telescope.actions').close,
-                }, -- n
-            } -- mappings
-        } -- defaults
-    }, -- opts
-} -- top
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "master",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
 
+    cmd = "Telescope",
+
+    keys = {
+      {
+        "<leader>ff",
+        function()
+          require("telescope.builtin").find_files()
+        end,
+        desc = "Find files",
+      },
+      {
+        "<leader>fg",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = "Live grep",
+      },
+      {
+        "<leader>fb",
+        function()
+          require("telescope.builtin").buffers()
+        end,
+        desc = "Find buffers",
+      },
+      {
+        "<leader>fh",
+        function()
+          require("telescope.builtin").help_tags()
+        end,
+        desc = "Find help",
+      },
+      {
+        "<leader>fr",
+        function()
+          require("telescope.builtin").oldfiles()
+        end,
+        desc = "Recent files",
+      },
+    },
+
+    opts = function()
+      local actions = require("telescope.actions")
+
+      return {
+        defaults = {
+          preview = {
+            treesitter = false,
+          },
+
+          mappings = {
+            i = {
+              ["<C-d>"] = actions.delete_buffer,
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+              ["<Esc>"] = actions.close,
+            },
+            n = {
+              ["d"] = actions.delete_buffer,
+              ["q"] = actions.close,
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+          },
+        },
+
+        pickers = {
+          find_files = {
+            follow = true,
+            hidden = true,
+          },
+          live_grep = {
+            follow = true,
+          },
+          buffers = {
+            sort_mru = true,
+            ignore_current_buffer = true,
+          },
+        },
+      }
+    end,
+  },
+}
